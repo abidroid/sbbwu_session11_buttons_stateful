87471ac12c139ee4855019eab7b57379bbd938c4
Abid <abidroid1@gmail.com>
2024-03-06 18:40:19 +0500

    custom button

--- lib/screens/button_screen.dart
+++ lib/screens/button_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:gap/gap.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -19,7 +23,9 @@ class ButtonScreen extends StatelessWidget {
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
                       foregroundColor: Colors.white),
-                  onPressed: () {},
+                  onPressed: () {
+                    print('User will be logged in ');
+                  },
                   child: const Text('Login'))),
           TextButton(onPressed: () {}, child: const Text('Sign Up')),
           OutlinedButton(onPressed: () {}, child: const Text('Submit')),
@@ -32,11 +38,78 @@ class ButtonScreen extends StatelessWidget {
               icon: const Icon(Icons.delete),
               label: const Text('Delete')),
           OutlinedButton.icon(
-              onPressed: () {},
+              onPressed: sendMessage,
               icon: const Icon(Icons.arrow_forward_ios),
-              label: const Text('Send'))
+              label: const Text('Send')),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              alignment: Alignment.center,
+              width: 200,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(colors: [
+                    Colors.pink,
+                    Colors.purple,
+                  ])),
+              child: const Text(
+                'Call',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              alignment: Alignment.center,
+              width: 200,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(colors: [
+                    Colors.pink,
+                    Colors.purple,
+                  ])),
+              child: const Text(
+                'Save',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          const Gap(20),
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(
+                  msg: 'Data will be deleted',
+                  fontSize: 30,
+                  backgroundColor: Colors.red);
+            },
+            child: Container(
+              alignment: Alignment.center,
+              width: 200,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: const LinearGradient(colors: [
+                    Colors.pink,
+                    Colors.purple,
+                  ])),
+              child: const Text(
+                'Delete',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  sendMessage() {
+    print('hi');
   }
 }
